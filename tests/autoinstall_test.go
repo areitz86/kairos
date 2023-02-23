5cc43b920eee49ec003ab4c08c0a91fe298d0f0d
Dimitris Karakasilis <dimitris@karakasilis.me>
2023-02-23 20:09:50 +0200

    Merge multiple similar tests into one
    because the big overhead comes from the BeforeEach block which is the
    same in all tests. The tests themselves are just checking differnet
    things on similar VMs. By merging them we same a lot of time in
    preparing the same env again and again.
    
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- tests/autoinstall_test.go
+++ tests/autoinstall_test.go
@@ -38,107 +38,109 @@ var _ = Describe("kairos autoinstall test", Label("autoinstall-test"), func() {
 			expectRebootedToActive(vm)
 		})
 
-		It("has grubenv file", func() {
-			out, err := vm.Sudo("cat /oem/grubenv")
-			Expect(err).ToNot(HaveOccurred(), out)
-			Expect(out).To(ContainSubstring("foobarzz"))
-		})
+		It("passes checks", func() {
+			By("checking grubenv file", func() {
+				out, err := vm.Sudo("cat /oem/grubenv")
+				Expect(err).ToNot(HaveOccurred(), out)
+				Expect(out).To(ContainSubstring("foobarzz"))
+			})
 
-		It("has custom cmdline", func() {
-			out, err := vm.Sudo("cat /proc/cmdline")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(out).To(ContainSubstring("foobarzz"))
-		})
+			By("checking custom cmdline", func() {
+				out, err := vm.Sudo("cat /proc/cmdline")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("foobarzz"))
+			})
 
-		It("uses the dracut immutable module", func() {
-			out, err := vm.Sudo("cat /proc/cmdline")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(out).To(ContainSubstring("cos-img/filename="))
-		})
+			By("checking the use of dracut immutable module", func() {
+				out, err := vm.Sudo("cat /proc/cmdline")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("cos-img/filename="))
+			})
 
-		It("installs Auto assessment", func() {
-			// Auto assessment was installed
-			out, _ := vm.Sudo("cat /run/initramfs/cos-state/grubcustom")
-			Expect(out).To(ContainSubstring("bootfile_loc"))
+			By("checking Auto assessment", func() {
+				// Auto assessment was installed
+				out, _ := vm.Sudo("cat /run/initramfs/cos-state/grubcustom")
+				Expect(out).To(ContainSubstring("bootfile_loc"))
 
-			out, _ = vm.Sudo("cat /run/initramfs/cos-state/grub_boot_assessment")
-			Expect(out).To(ContainSubstring("boot_assessment_blk"))
+				out, _ = vm.Sudo("cat /run/initramfs/cos-state/grub_boot_assessment")
+				Expect(out).To(ContainSubstring("boot_assessment_blk"))
 
-			cmdline, _ := vm.Sudo("cat /proc/cmdline")
-			Expect(cmdline).To(ContainSubstring("rd.emergency=reboot rd.shell=0 panic=5"))
-		})
+				cmdline, _ := vm.Sudo("cat /proc/cmdline")
+				Expect(cmdline).To(ContainSubstring("rd.emergency=reboot rd.shell=0 panic=5"))
+			})
 
-		It("has writeable tmp", func() {
-			_, err := vm.Sudo("echo 'foo' > /tmp/bar")
-			Expect(err).ToNot(HaveOccurred())
+			By("checking writeable tmp", func() {
+				_, err := vm.Sudo("echo 'foo' > /tmp/bar")
+				Expect(err).ToNot(HaveOccurred())
 
-			out, err := Machine.Command("sudo cat /tmp/bar")
-			Expect(err).ToNot(HaveOccurred())
+				out, err := Machine.Command("sudo cat /tmp/bar")
+				Expect(err).ToNot(HaveOccurred())
 
-			Expect(out).To(ContainSubstring("foo"))
-		})
+				Expect(out).To(ContainSubstring("foo"))
+			})
 
-		It("has bpf mount", func() {
-			out, err := vm.Sudo("mount")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(out).To(ContainSubstring("bpf"))
-		})
+			By("checking bpf mount", func() {
+				out, err := vm.Sudo("mount")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("bpf"))
+			})
 
-		It("has correct permissions", func() {
-			out, err := vm.Sudo(`stat -c "%a" /oem`)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(out).To(ContainSubstring("770"))
+			By("checking correct permissions", func() {
+				out, err := vm.Sudo(`stat -c "%a" /oem`)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("770"))
 
-			out, err = vm.Sudo(`stat -c "%a" /usr/local/cloud-config`)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(out).To(ContainSubstring("770"))
-		})
+				out, err = vm.Sudo(`stat -c "%a" /usr/local/cloud-config`)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("770"))
+			})
 
-		It("has grubmenu", func() {
-			out, err := vm.Sudo("cat /run/initramfs/cos-state/grubmenu")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(out).To(ContainSubstring("state reset"))
-		})
+			By("checking grubmenu", func() {
+				out, err := vm.Sudo("cat /run/initramfs/cos-state/grubmenu")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("state reset"))
+			})
 
-		It("has additional mount specified, with no dir in rootfs", func() {
-			out, err := vm.Sudo("mount")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(out).To(ContainSubstring("/var/lib/longhorn"))
-		})
+			By("checking additional mount specified, with no dir in rootfs", func() {
+				out, err := vm.Sudo("mount")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("/var/lib/longhorn"))
+			})
 
-		It("has rootfs shared mount", func() {
-			out, err := vm.Sudo(`cat /proc/1/mountinfo | grep ' / / '`)
-			Expect(err).ToNot(HaveOccurred(), out)
-			Expect(out).To(ContainSubstring("shared"))
-		})
+			By("checking rootfs shared mount", func() {
+				out, err := vm.Sudo(`cat /proc/1/mountinfo | grep ' / / '`)
+				Expect(err).ToNot(HaveOccurred(), out)
+				Expect(out).To(ContainSubstring("shared"))
+			})
 
-		It("doesn't has grub data into the cloud config", func() {
-			out, err := vm.Sudo(`cat /oem/90_custom.yaml`)
-			Expect(err).ToNot(HaveOccurred(), out)
-			Expect(out).ToNot(ContainSubstring("vga_text"))
-			Expect(out).ToNot(ContainSubstring("videotest"))
-		})
+			By("checking that it doesn't has grub data into the cloud config", func() {
+				out, err := vm.Sudo(`cat /oem/90_custom.yaml`)
+				Expect(err).ToNot(HaveOccurred(), out)
+				Expect(out).ToNot(ContainSubstring("vga_text"))
+				Expect(out).ToNot(ContainSubstring("videotest"))
+			})
 
-		It("has corresponding state", func() {
-			out, err := vm.Sudo("kairos-agent state")
-			Expect(err).ToNot(HaveOccurred())
-			fmt.Println(out)
-			Expect(out).To(ContainSubstring("boot: active_boot"))
+			By("checking corresponding state", func() {
+				out, err := vm.Sudo("kairos-agent state")
+				Expect(err).ToNot(HaveOccurred())
+				fmt.Println(out)
+				Expect(out).To(ContainSubstring("boot: active_boot"))
 
-			stateAssertVM(vm, "oem.mounted", "true")
-			stateAssertVM(vm, "oem.found", "true")
-			stateAssertVM(vm, "persistent.mounted", "true")
-			stateAssertVM(vm, "state.mounted", "true")
-			stateAssertVM(vm, "oem.type", "ext4")
-			stateAssertVM(vm, "persistent.type", "ext4")
-			stateAssertVM(vm, "state.type", "ext4")
-			stateAssertVM(vm, "oem.mount_point", "/oem")
-			stateAssertVM(vm, "persistent.mount_point", "/usr/local")
-			stateAssertVM(vm, "persistent.name", "/dev/vda")
-			stateAssertVM(vm, "state.mount_point", "/run/initramfs/cos-state")
-			stateAssertVM(vm, "oem.read_only", "false")
-			stateAssertVM(vm, "persistent.read_only", "false")
-			stateAssertVM(vm, "state.read_only", "true")
+				stateAssertVM(vm, "oem.mounted", "true")
+				stateAssertVM(vm, "oem.found", "true")
+				stateAssertVM(vm, "persistent.mounted", "true")
+				stateAssertVM(vm, "state.mounted", "true")
+				stateAssertVM(vm, "oem.type", "ext4")
+				stateAssertVM(vm, "persistent.type", "ext4")
+				stateAssertVM(vm, "state.type", "ext4")
+				stateAssertVM(vm, "oem.mount_point", "/oem")
+				stateAssertVM(vm, "persistent.mount_point", "/usr/local")
+				stateAssertVM(vm, "persistent.name", "/dev/vda")
+				stateAssertVM(vm, "state.mount_point", "/run/initramfs/cos-state")
+				stateAssertVM(vm, "oem.read_only", "false")
+				stateAssertVM(vm, "persistent.read_only", "false")
+				stateAssertVM(vm, "state.read_only", "true")
+			})
 		})
 	})
 })
